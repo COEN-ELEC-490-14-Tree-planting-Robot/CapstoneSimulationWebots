563a6f470fbd1af90abc1664efdc11bbc2f34032
Haiyi Zhou <haiyi@genielab.co>
2021-01-18 14:38:24 -0500

    init

---
+++ controllers/sojourner_custom/build/release/sojourner_custom.d
@@ -0,0 +1,6 @@
+build/release/sojourner_custom.o: sojourner_custom.c \
+ E:\Webots/include/controller/c/webots/keyboard.h \
+ E:\Webots/include/controller/c/webots/types.h \
+ E:\Webots/include/controller/c/webots/motor.h \
+ E:\Webots/include/controller/c/webots/robot.h \
+ E:\Webots/include/controller/c/webots/nodes.h
